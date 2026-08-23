27589ffcbb50b6f5d5ff0440906ff9baa9ed3014
Sameeksha Sharma <sameekshasharma017@gmail.com>
2026-08-23 16:58:02 +0530

    added hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+
+import "dotenv/config";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+export default {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: { enabled: true, runs: 200 }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    }
+  }
+};
